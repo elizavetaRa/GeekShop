bca131771f31ce2b53d0519243935185eea30b14
h4llow3En <Doering.Felix@googlemail.com>
2014-11-18 10:31:34 +0100

    Login-Seite hinzugefügt. Für den Prototypen

---
+++ src/main/java/GeekShop/web/LoginController.java
@@ -0,0 +1,40 @@
+package GeekShop.web;
+
+/**
+ * Created by h4llow3En on 17/11/14.
+ */
+
+import GeekShop.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+public class LoginController {
+
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "/index";
+    }
+
+    @RequestMapping(value = "/index")
+    public String login(@RequestParam("userName") String userName, @RequestParam("password") String password){
+        if (userName == "" || password == "") {
+            return "redirect:/index";
+        } else{
+            return "redirect:/main";
+        }
+
+    }
+}
