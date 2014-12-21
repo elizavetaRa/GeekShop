7f39d8a4b978b2f8c1e1e8447df354cc6678e3ed
Elizaveta Ragozina <elizaveta93@web.de>
2014-12-21 22:38:45 +0100

    nicht funktionierender GSCart

---
+++ src/main/java/geekshop/model/GSCart.java
@@ -0,0 +1,33 @@
+package geekshop.model;
+
+import org.salespointframework.order.Cart;
+
+
+/**
+ * Created by Lisa on 21.12.2014.
+ */
+public class GSCart extends Cart {
+
+    private boolean reclaimModus;
+
+    public GSCart(){
+        super() ;
+        this.reclaimModus=true;
+
+    }
+
+    public boolean isReclaimModus() {
+        return reclaimModus;
+    }
+
+    public void switchModus(){
+
+        if (this.reclaimModus==true){this.reclaimModus=false;}
+        else this.reclaimModus=true;
+
+    }
+
+
+}
+
+
