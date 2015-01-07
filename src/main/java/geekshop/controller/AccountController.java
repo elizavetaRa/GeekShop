d7824f8b4738ba7effb29881de0c5b1f834e6908
Sebastian <Sebastian.Doering@mailbox.tu-dresden.de>
2015-01-07 22:19:01 +0100

    AccountController-Tests I

--- src/main/java/geekshop/controller/AccountController.java
+++ src/main/java/geekshop/controller/AccountController.java
@@ -93,19 +93,18 @@ class AccountController {
         }
 
         // arrange new welcome joke
-        String sessionId = user.getCurrentSessionId();
         List<Joke> recentJokes = user.getRecentJokes();
 
-        if (session.getId().equals(sessionId)) {
+        if (session.getAttribute("jokeDisplayed") != null && (boolean)session.getAttribute("jokeDisplayed")) {
             model.addAttribute("joke", user.getLastJoke());
         } else {
             Joke joke = getRandomJoke(recentJokes);
 
             if (joke != null)
                 user.addJoke(joke);
-
-            user.setCurrentSessionId(session.getId());
             userRepo.save(user);
+
+            session.setAttribute("jokeDisplayed", true);
 
             model.addAttribute("joke", joke);
         }
@@ -115,7 +114,7 @@ class AccountController {
     /**
      * Determines a random {@link Joke} out of the {@link JokeRepository} which is not contained in the list of recent jokes.
      */
-    private Joke getRandomJoke(List<Joke> recentJokes) {
+    public Joke getRandomJoke(List<Joke> recentJokes) {
         List<Joke> allJokes = new LinkedList<Joke>();
         for (Joke j : jokeRepo.findAll()) {
             allJokes.add(j);
---
+++ src/test/java/geekshop/controller/AccountControllerAuthenticationTests.java
@@ -0,0 +1,128 @@
+package geekshop.controller;
+
+import geekshop.AbstractWebIntegrationTests;
+import geekshop.model.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.salespointframework.useraccount.AuthenticationManager;
+import org.salespointframework.useraccount.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpSession;
+import java.util.*;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class AccountControllerAuthenticationTests extends AbstractWebIntegrationTests {
+
+    @Autowired
+    private AccountController controller;
+    @Autowired
+    private UserRepository userRepo;
+    @Autowired
+    private MessageRepository messageRepo;
+    @Autowired
+    private JokeRepository jokeRepo;
+    @Autowired
+    private AuthenticationManager authManager;
+    @Autowired
+    private HttpSession session;
+
+    private User user;
+
+
+    @Before
+    public void setUp() {
+        super.setUp();
+
+        login("owner", "123");
+        user = userRepo.findByUserAccount(authManager.getCurrentUser().get());
+    }
+
+
+    @Test
+    public void testIndexOwner() {
+        Model model = new ExtendedModelMap();
+
+        String view = controller.index(model, Optional.of(user.getUserAccount()), session);
+
+        assertEquals(user, session.getAttribute("user"));
+        assertEquals(messageRepo, session.getAttribute("msgRepo"));
+        assertTrue((boolean) session.getAttribute("isReclaim"));
+        assertTrue((boolean) session.getAttribute("overview"));
+
+        assertTrue(messageRepo.findByMessageKind(MessageKind.PASSWORD).iterator().hasNext());
+
+        assertEquals("welcome", view);
+        assertTrue(model.containsAttribute("joke"));
+
+        // test whether password message to owner is sent only once, and whether a new joke is arranged only once a session
+        Joke lastJoke = user.getLastJoke();
+        controller.index(model, Optional.of(user.getUserAccount()), session);
+        assertEquals(lastJoke, model.asMap().get("joke"));
+        Iterator<Message> iterator = messageRepo.findByMessageKind(MessageKind.PASSWORD).iterator();
+        assertTrue(iterator.next() != null);
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void testIndexEmployee() {
+        login("hans", "123");
+
+        // Hans has an insecure password.
+        Model model = new ExtendedModelMap();
+
+        User hans = userRepo.findByUserAccount(authManager.getCurrentUser().get());
+
+        String view = controller.index(model, Optional.of(hans.getUserAccount()), session);
+
+        assertEquals(hans, session.getAttribute("user"));
+        assertEquals(messageRepo, session.getAttribute("msgRepo"));
+        assertTrue((boolean) session.getAttribute("isReclaim"));
+        assertTrue((boolean) session.getAttribute("overview"));
+
+        assertEquals("adjustpw", view);
+        assertThat(hans.getUserAccount().getRoles(), hasItem(new Role("ROLE_INSECURE_PASSWORD")));
+        assertTrue(model.containsAttribute("passwordRules"));
+
+        assertFalse(model.containsAttribute("joke"));
+
+        // Hans adjustes his password.
+        model = new ExtendedModelMap();
+
+        controller.adjustPW(model, "!A2a$sA3", "!A2a$sA3", Optional.of(hans.getUserAccount()));
+
+        view = controller.index(model, Optional.of(hans.getUserAccount()), session);
+
+        assertThat(hans.getUserAccount().getRoles(), not(hasItem(new Role("ROLE_INSECURE_PASSWORD"))));
+
+        assertEquals("welcome", view);
+        assertTrue(model.containsAttribute("joke"));
+    }
+
+    @Test
+    public void testGetRandomJoke() {
+        jokeRepo.deleteAll();
+        assertNull(controller.getRandomJoke(new LinkedList<Joke>()));
+
+        Joke j1 = new Joke("joke1");
+        Joke j2 = new Joke("joke2");
+        Joke j3 = new Joke("joke3");
+        Joke j4 = new Joke("joke4");
+        Joke j5 = new Joke("joke5");
+        Joke j6 = new Joke("joke6");
+        jokeRepo.save(Arrays.asList(j1, j2, j3, j4, j5, j6));
+
+        List<Joke> recent = Arrays.asList(j1, j2, j3);
+        assertThat(controller.getRandomJoke(recent), anyOf(Arrays.asList(is(j4), is(j5), is(j6))));
+
+        recent = Arrays.asList(j1, j2, j3, j4, j5);
+        assertEquals(controller.getRandomJoke(recent), j6);
+
+        recent = Arrays.asList(j2, j1, j3, j4, j5, j6);
+        assertEquals(j2, controller.getRandomJoke(recent));
+    }
+}
